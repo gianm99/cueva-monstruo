79b0a459e4342a7dac4e3d07f42c144b329acd7b
gianm99 <gianlucasmartinchamorro@gmail.com>
2020-12-05 18:45:43 +0100

    Crear el proyecto en NetBeans

---
+++ CuevaDelMonstruo/src/cuevadelmonstruo/CuevaDelMonstruo.java
@@ -0,0 +1,16 @@
+package cuevadelmonstruo;
+
+/**
+ *
+ * @author gianm
+ */
+public class CuevaDelMonstruo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
