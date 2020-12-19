a40b46dbfb2c9fd4a6bd3f66b23bc05f394dd265
gianm99 <gianlucasmartinchamorro@gmail.com>
2020-12-19 21:26:20 +0100

    Cambiar el color de fondo del menú

--- CuevaDelMonstruoLibGDX/core/src/com/cueva/monstruo/screens/MenuScreen.java
+++ CuevaDelMonstruoLibGDX/core/src/com/cueva/monstruo/screens/MenuScreen.java
@@ -63,7 +63,7 @@ public class MenuScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1f, 1f, 1f, 1);
+		Gdx.gl.glClearColor(251f / 255f, 140f / 255f, 100f / 255f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
 		stage.draw();
